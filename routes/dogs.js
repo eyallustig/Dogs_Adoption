4db518aa1808fc8ce3dec34a3f1640e7e8eea213
Eyal Lustig <Eyallustig@gmail.com>
2019-10-24 17:18:49 +0300

    add db

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,9 +1,19 @@
 const express = require("express"),
+    Dog = require("../models/dog"),
     router = express.Router();
 
-// INDEX - show all pets
+// INDEX - show all dogs
 router.get('/', (req, res) => {
-    res.render("dogs/index")
+    Dog.find({}, (err, allDogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(allDogs);
+            res.render("dogs/index", {
+                dogs: allDogs
+            });
+        }
+    });
 });
 
 // NEW - show form to create new pet
@@ -13,10 +23,17 @@ router.get('/new', (req, res) => {
 
 // CREATE - Create a new dog, then redirect somewhere
 router.post('/', (req, res) => {
-    console.log(req.body.dog);
+    let newDog = req.body.dog;
+    console.log(newDog);
     // create a new dog and save to DB
-
-    res.redirect("/dogs");
+    Dog.create(newDog, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`Created dog: ${newlyCreated.name}`);
+            res.redirect("/dogs");
+        }
+    })
 });
 
 module.exports = router;
